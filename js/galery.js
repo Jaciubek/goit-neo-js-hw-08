3287e5d019d2271b042eb2c1cab047b806ab3778
Jaciubek <slawomir.jaciubek@gmail.com>
2024-10-30 21:48:49 +0100

    do poprawy

--- js/galery.js
+++ js/galery.js
@@ -1,20 +1,23 @@
-import { images } from './images.js';
+// import { images } from './images.js';
 
-const gallery = document.querySelector('.gallery');
-const markup = images
-    .map((image) => `  <li class="gallery__item">
-                        <a class="gallery__link" href="${image.original}">
-                            <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
-                        </a>
-                    </li>`
-  )
-    .join("");
+// const gallery = document.querySelector('.gallery');
+// const markup = images
+//     .map((image) => `  <li class="gallery__item">
+//                         <a class="gallery__link" href="${image.original}">
+//                             <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+//                         </a>
+//                     </li>`
+//   )
+//     .join("");
   
-gallery.insertAdjacentHTML("afterbegin", markup);
+// gallery.insertAdjacentHTML("afterbegin", markup);
 
-const lightbox = new SimpleLightbox('.gallery__link', {
-    captionsData: "alt",
-    captionDelay: 250,
-});
+// const lightbox = new SimpleLightbox('.gallery__link', {
+//     captionsData: "alt",
+//     captionDelay: 250,
+// });
 
-console.log(images);
+// console.log(images);
+
+
+// do poprawy
